db72b697ce89690cdc80bf36d7db2ea6c7a44882
INazca <billy78@gmx.net>
2020-02-12 13:15:58 +0100

    Setup simple server
    A simple version of the server is setup upon which the home page can be accessed and also connecting to other routes is already possible but won't lead to any outcome.

---
+++ app.js
@@ -0,0 +1,25 @@
+/* eslint-env node */
+//these imports will only work if you use the self specified server start "npm start"
+import ServerConst from "./lib/utils/ServerConst.js";
+
+var express = require("express"),
+    app = express();
+
+app.use(express.static("app/rescources"));
+app.set("view engine", "ejs");
+app.set("views", "app/views");
+
+//serve the home page on the main route
+app.get("/", function (req, res) {
+    res.render("index");
+});
+
+//if the user connects on a valid code then he will be given the needed information for taking part in a survey
+//the code is representative for the id of the study participant
+app.get("/:code", function (req, res) {
+    res.send(req.params.code);
+});
+
+app.listen(ServerConst.port, function () {
+    console.log("Serving homepage on http://localhost:" + ServerConst.port + "/");
+});
