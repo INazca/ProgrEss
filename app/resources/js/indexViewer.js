57cb7bafb80c1c22b1fb2846c56bafb54d96ddc9
INazca <billy78@gmx.net>
2020-02-14 11:24:57 +0100

    Add first prototypal version of the syntax highlighting evaluate view

--- app/resources/js/indexViewer.js
+++ app/resources/js/indexViewer.js
@@ -1,4 +1,14 @@
-var codeEditor = CodeMirror(document.getElementsByClassName("editor-container-ss")[0], {
+var codeEditorSS = CodeMirror(document.getElementsByClassName("editor-container-ss")[0], {
+    value: "public class Object {\n    public Object(int variable) {\n    }\n}",
+    mode: "text/x-java",
+    readOnly: true,
+}),
+codeEditorSE = CodeMirror(document.getElementsByClassName("editor-container-se")[0], {
+    value: "public class Object {\n    public Object(int variable) {\n    }\n}",
+    mode: "text/x-java",
+    readOnly: true,
+}),
+codeEditorSD = CodeMirror(document.getElementsByClassName("editor-container-sd")[0], {
     value: "public class Object {\n    public Object(int variable) {\n    }\n}",
     mode: "text/x-java",
     readOnly: true,
